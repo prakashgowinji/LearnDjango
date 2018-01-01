5a7ea79c6580b1a3c766e893e4444a0b3625aeab
Prakash Karuppannan Govindaraj <prakashkarupannan.govindaraj@impigertech.com>
2018-01-01 12:58:37 +0530

    Added a form named form_page.html in views.py

--- play_ground/first_app/views.py
+++ play_ground/first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Topic, AccessRecord, WebPage
+from . import forms
 # Create your views here.
 
 def index(request):
@@ -9,3 +10,18 @@ def index(request):
 
     mydict = {'insert_content': "Hello, I am from first App"}
     return render(request, 'first_app/index.html', context=date_dict)
+
+def form_name_view(request):
+    form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            #DO Something Code
+            print("Validation Success!")
+            print("Name: "+(form.cleaned_data['name']))
+            print("Email: "+(form.cleaned_data['email']))
+            print("Text: "+(form.cleaned_data['text']))
+
+    return render(request,'first_app/form_page.html', {'form':form})
